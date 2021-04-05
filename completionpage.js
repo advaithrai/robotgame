746cfae3dec671ab126659bb0de583e25ebad682
Advaith Rai <advaith_rai@yahoo.com>
2021-04-05 12:31:03 -0500

    edited the game over and completion screen

--- completionpage.js
+++ completionpage.js
@@ -8,17 +8,26 @@ test.completionpage.prototype = {
          game.load.image('bg', 'assets/backgrounds/dirty-wall.jpg');
          game.load.image('complete', 'assets/sprites/levelcomplete.png');
          
+         game.load.image('gear', 'assets/sprites/gear.png');
+          game.load.image('bullet', 'assets/sprites/bullet.png');
+         
      },
     
      create: function() {
          
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        game.stage.backgroundColor = '#000000'
          
-         var background = game.add.sprite(0,0, 'bg');
+   //      var background = game.add.sprite(0,0, 'bg');
          var logo = game.add.sprite(100,500, 'complete');
          
-        gearLogo = game.add.text(900,800, 'Gears Collected: ' + gearCnt, {fontSize: 800, fill:'#DA420A'});
-        ammoLogo = game.add.text(900,900, 'Ammo Left: ' + ammo, {fontSize: 800, fill:'#DA420A'});
+        gearLogo = game.add.text(600,700, 'Gears Collected: ' + gearCnt, {fontSize: 1000, fill:'#DA420A'});
+        ammoLogo = game.add.text(600,800, 'Ammo Left: ' + ammo, {fontSize: 1000, fill:'#DA420A', fontFamily: 'Copperplate'});
+         
+        var gear_1 = game.add.sprite(500,680, 'gear');
+        var bullet_1 = game.add.sprite(500,805, 'bullet');
+        bullet_1.scale.setTo(4,4);
 
      },
     
