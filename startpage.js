06ed1596f04d7066bee202281fd721b7836b0dde
JoonsungHa <43160111+JoonsungHa@users.noreply.github.com>
2021-04-13 22:14:16 -0700

    Add files via upload

--- startpage.js
+++ startpage.js
@@ -28,7 +28,8 @@ test.startpage.prototype = {
          var wad = game.add.sprite(1100, 100, 'wad');
          var mouse = game.add.sprite(1100, 200, 'mouse');
          
-         var playButton = game.add.button(500,700, 'play', function() { game.state.start('levelone'); player_health = 100; gearCnt = 0; ammo = 10;})
+         //storypage
+         var playButton = game.add.button(500,700, 'play', function() { game.state.start('storypage'); player_health = 100; gearCnt = 0; ammo = 10;})
          
          playButton.scale.setTo(2.5,2.5);
      },
