8fc7fe2dcdd4203f36d0fb442574494537ca7f82
JoonsungHa <43160111+JoonsungHa@users.noreply.github.com>
2021-04-26 09:19:53 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var game = new Phaser.Game(1500,1000, Phaser.AUTO);
 game.state.add('levelone', test.levelone);
-game.state.add('levelotwo', test.leveltwo);
+game.state.add('leveltwo', test.leveltwo);
 game.state.add('levelthree', test.levelthree);
 game.state.add('startpage', test.startpage);
 game.state.add('completionpage', test.completionpage);
+game.state.add('completionpage1', test.completionpage1);
 game.state.add('storypage', test.storypage);
 game.state.add('gameover', test.gameover);
-game.state.start('levelthree');
+game.state.start('startpage');
